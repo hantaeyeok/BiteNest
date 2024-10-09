f7a3d77aacfa2cb1de38206d2b8a55eece3aeb3e
leeyangie <prumpy11@naver.com>
2024-10-09 14:23:28 +0900

    Reapply "Reapply "뭐징""
    This reverts commit bc10700c576c0d45dd349f5df6747d60a3ab7ee8.

---
+++ biteNest/src/main/frontend/src/components/fridge/shared/AddBanner 2.tsx
@@ -0,0 +1,4 @@
+function AddBanner() {
+  return <div className="w-full h-[100px] bg-green-300 rounded-lg"></div>
+}
+export default AddBanner
---
+++ biteNest/src/main/frontend/tailwind.config 2.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors: {
+        brown: {
+          100: '#A0522D',
+          200: '#DEB887',
+          300: '#8B4513',
+        },
+        salmon: {
+          100: '#FFA07A',
+          200: '#FFDAB9',
+        },
+        caramel: '#CD853F',
+        creamWhite: '#FFF9E5',
+        darkCream: '#e0cbb6',
+        cream: '#F7ECE1',
+        textBrown: {
+          100: '#3F2514',
+          200: '#6D4226',
+        },
+        green: {
+          100: '#789C00',
+          200: '#435700',
+          300: '#effacd',
+        },
+        red: '#FF6243',
+        yellow: '#FFD600',
+        blue: '#437FA1',
+      },
+      fontFamily: {
+        logo: ['MapoHongdaeFreedom', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'], // You can customize this further
+      },
+    },
+  },
+  plugins: [],
+}
