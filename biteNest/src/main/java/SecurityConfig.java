8dbd50a0994c7c143f16b63abe4bd4aadc4cc92d
jwj4546 <83587579+jwj4546@users.noreply.github.com>
2024-10-10 23:56:54 +0900

    SecurityConfig 경로 변경 & Token 사용
    SecurityConfig 경로 변경 & Token 사용

--- biteNest/src/main/java/SecurityConfig.java
+++ biteNest/src/main/java/SecurityConfig.java
@@ -1,5 +1,5 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,14 +8,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())  // CSRF 비활성화
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()  // 모든 요청 허용
-            );
+            .antMatchers("/api/login").permitAll()  // 로그인 API는 인증 없이 접근 가능
+            .anyRequest().authenticated()  // 그 외 요청은 인증 필요
+            )
+            .apply(new JwtConfigurer(jwtTokenProvider));  // JWT 필터 적용
         return http.build();
     }
 }
